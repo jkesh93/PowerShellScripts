91171e232e77127636cde5ec2e261e0bc2c356dd
jkesh93 <jay111893@gmail.com>
2019-11-23 09:43:32 -0800

    Graph API Script courtesy of Microsoft

---
+++ AzureScripts/Graph_Get_Specified_Intune_Devices/Get_Specified_Intune_Devices.ps1
@@ -0,0 +1,18 @@
+######################
+### What this does ###
+######################
+<#
+.DESCRIPTION
+1. Authenticates into the Graph API by getting the token
+2. Pulls Azure Devices managed by Intune
+3. Filters based on details
+4. Outputs to CSV
+
+#>
+######################
+###    Variables   ###
+######################
+
+######################
+###     Script     ###
+######################
