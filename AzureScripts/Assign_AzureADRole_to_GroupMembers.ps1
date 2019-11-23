16bd4d1ca58ca8a365f3c73c5ed1751f79de71bd
Jay <jay111893@gmail.com>
2019-11-22 17:23:57 -0800

    Update Assign_AzureADRole_to_GroupMembers.ps1
    Neatened up some of the code

--- AzureScripts/Assign_AzureADRole_to_GroupMembers.ps1
+++ AzureScripts/Assign_AzureADRole_to_GroupMembers.ps1
@@ -10,25 +10,25 @@
 ###    Variables   ###
 ######################
 # SET GROUP NAME
-$groupName = '';
+$groupName = "TEST - Jay's Test Group";
 
 # SET LOG EXPORT PATH
-$exportPath = '.\roleAssignmentLog.csv'
+$exportPath = '.\roleAssignmentLog.csv';
 
 # SET ROLE TO ASSIGN
-$roleToAssign = '';
+$roleToAssign = 'Authentication Administrator';
 
 # LOGGING ON/OFF: 0 for off, 1 for on
-$loggingSwitch = 0;
+$loggingSwitch = 1;
 
 ######################
 ###     Script     ###
 ######################
 
 # Create Log file...
-Add-Content -Path $exportPath -Value "ObjectId,DisplayName,UserPrincipalName,UserType,Role"
+Add-Content -Path $exportPath -Value "ObjectId,DisplayName,UserPrincipalName,UserType,Role";
 
-Connect-AzureAD
+#Connect-AzureAD
 
 # If $groupName is not set
 if($groupName -eq '')
@@ -43,38 +43,44 @@ if($roleToAssign -eq '')
 }
 
 # Get Azure AD Group Members
-$group = Get-AzureADGroup -SearchString $groupName | Select-Object ObjectId
-$groupId = $group.ObjectId
+$group = Get-AzureADGroup -SearchString $groupName | Select-Object ObjectId;
+$groupId = $group.ObjectId;
 $groupMembers = Get-AzureADGroupMember -ObjectId $groupId;
 
 # Fetch $roleToAssign role instance
-$role = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq $roleToAssign}
+$role = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq $roleToAssign};
 
 # If role instance does not exist, cancel the operation...
 if ($role -eq $null)
 {
-    Write-Host -ForegroundColor Yellow "Role does not exist. Cancelling..."
+    Write-Host -ForegroundColor Yellow "Role does not exist. Cancelling...";
 }
 else
 {
     # Foreach group member
     foreach($member in $groupMembers)
     {
-        $oid = $member.ObjectId
-        $dn = $member.DisplayName
-        $upn = $member.UserPrincipalName
-        $ut = $member.UserType
+        $oid = $member.ObjectId;
+        $dn = $member.DisplayName;
+        $upn = $member.UserPrincipalName;
+        $ut = $member.UserType;
 
         # Add user to role
-        Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $oid
-
+        try
+        {
+            Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $oid;
+        }
+        catch
+        {
+            Write-Host -ForegroundColor Red "Unable to add user $dn";
+        }
         # Fetch role membership for role to confirm
-        Get-AzureADDirectoryRoleMember -ObjectId $role.ObjectId | Get-AzureADUser
+        Get-AzureADDirectoryRoleMember -ObjectId $role.ObjectId | Get-AzureADUser;
 
         # If logging, log...
         if($loggingSwitch -eq 1)
         {
-            Add-Content -Path $exportPath -Value "$oid,$dn,$upn,$ut,$roleToAssign"
+            Add-Content -Path $exportPath -Value "$oid,$dn,$upn,$ut,$roleToAssign";
         }
     }
 }
