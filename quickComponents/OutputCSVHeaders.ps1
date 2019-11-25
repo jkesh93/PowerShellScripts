2241d572db07b1686a495a450ecf95353d801988
Jay <jay111893@gmail.com>
2019-11-25 14:34:15 -0800

    Create OutputCSVHeaders.ps1

---
+++ quickComponents/OutputCSVHeaders.ps1
@@ -0,0 +1,27 @@
+######################
+### What this does ###
+######################
+# Take in a CSV
+# Creates array from headers
+# Export headers as CSV
+
+######################
+###    Variables   ###
+######################
+$csvFile = '.\Intune_Managed_Devices.csv';
+$csvOutput = '.\OutputFile.csv';
+$headersArrayList = [System.Collections.ArrayList]@();
+$returnedRowsArrayList = [System.Collections.ArrayList]@();
+
+######################
+###     Script     ###
+######################
+
+# Take in a CSV
+$csvFile = Import-Csv $csvFile;
+
+# Create array from headers
+$headers = $csvFile | Get-member -MemberType 'NoteProperty' | Select-Object -ExpandProperty 'Name'
+
+# Export headers as CSV
+$headers | Export-Csv -Path $csvOutput -NoTypeInformation
