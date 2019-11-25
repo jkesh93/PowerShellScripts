ac78d03989cea0982c195fb613f0805d72a6fb92
Jay <jay111893@gmail.com>
2019-11-25 11:09:36 -0800

    Update PowerShellArrayList.ps1

--- quickComponents/PowerShellArrayList.ps1
+++ quickComponents/PowerShellArrayList.ps1
@@ -1,46 +1,14 @@
 # Create the array
-$MyArrayList = [System.Collections.ArrayList]@();
+$arrayList = [System.Collections.ArrayList]@();
 
-# Add to the array
-$firstName = "John";
-$lastName = "Smith";
-$person = @($firstName, $lastName);
-$MyArrayList.Add($person);
+function addToArrayList($key,$val)
+{
+    $obj = @($key, $val);
+    $arrayList.Add($obj)
+}
 
-    # Show the array list
-    $MyArrayList;
-        # returns
-        <#####
-        PS C:\Users\YourUser\ScriptPath>
-            0
-            John
-            Smith
-        ######>
+addToArrayList -key 'dog' -val 'mickey';
+addToArrayList -key 'cat' -val 'tiffany';
+addToArrayList -key 'person' -val 'jay';
 
-# Get an item from the array list
-    # Only the first name
-    $MyArrayList[0][0]
-        # returns
-        <#####
-        PS C:\Users\YourUser\ScriptPath>
-            0
-            John
-        ######>
-
-    # Only the last name
-    $MyArrayList[0][1]
-        # returns
-        <#####
-        PS C:\Users\YourUser\ScriptPath>
-            0
-            Smith
-        ######>
-
-# Get the length of the array list
-    $MyArrayList.length
-        # returns
-        <#####
-        PS C:\Users\YourUser\ScriptPath>
-            0
-            2
-        ######>
+$arrayList[0][0]
