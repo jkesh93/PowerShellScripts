e64b6b87af2b0450ba4e419fc9590ef1dd4b0cc2
Jay <jay111893@gmail.com>
2020-08-04 20:06:18 -0700

    Update scriptTemplate.ps1
    Added output.csv

--- Templates/scriptTemplate.ps1
+++ Templates/scriptTemplate.ps1
@@ -1,10 +1,20 @@
 #############################
 # Inputs / Variables
 #############################
-
-
+$csvOutputName = ".\output.csv"
 
 #############################
 # Logic
 #############################
 
+## remove old file if it exists
+if(Test-Path $csvOutputName)
+{
+"File exists"
+rm $csvOutputName
+}
+else
+{
+"File does not exist"
+Add-Content $csvOutputName -Value "Hola"
+}
